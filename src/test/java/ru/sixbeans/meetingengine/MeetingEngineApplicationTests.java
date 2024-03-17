42fae8c437e73554ffff70141ed80766dac9b7f8
Nozistance <nozistance@gmail.com>
2024-03-17 19:29:13 +0300

    Initial commit

---
+++ src/test/java/ru/sixbeans/meetingengine/MeetingEngineApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.sixbeans.meetingengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
