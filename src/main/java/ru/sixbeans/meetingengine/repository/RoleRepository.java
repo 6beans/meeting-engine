57396e6849c5f0d3e4bb1a41719c19214ebedb5a
Nozistance <nozistance@gmail.com>
2024-04-18 21:39:01 +0300

    Remove unused Role entity + Repository

--- src/main/java/ru/sixbeans/meetingengine/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.sixbeans.meetingengine.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.sixbeans.meetingengine.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
