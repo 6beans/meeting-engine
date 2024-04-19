f8f22665a492c730a2984d2b3c017553d2508bb2
Nozistance <nozistance@gmail.com>
2024-04-19 09:13:37 +0300

    Modify User entity for only OpenID Connect use cases

--- src/main/java/ru/sixbeans/meetingengine/repository/UserRepository.java
+++ src/main/java/ru/sixbeans/meetingengine/repository/UserRepository.java
@@ -3,9 +3,7 @@ package ru.sixbeans.meetingengine.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.sixbeans.meetingengine.entity.User;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUsername(String username);
+    boolean existsBySub(String sub);
 }
