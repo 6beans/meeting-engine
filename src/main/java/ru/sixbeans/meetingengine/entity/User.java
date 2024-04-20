e936f0cfa99ab178027a79c5265d99b23e5d6d29
Nozistance <nozistance@gmail.com>
2024-04-20 16:45:59 +0300

    Remove entity name conflict

--- src/main/java/ru/sixbeans/meetingengine/entity/User.java
+++ src/main/java/ru/sixbeans/meetingengine/entity/User.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "users")
 public class User {
 
     @Id
@@ -35,7 +36,6 @@ public class User {
     private String email;
 
     @Lob
-    @Column(columnDefinition="BLOB")
     private byte[] avatar;
 
     @Column(columnDefinition = "DATE")
