f35bed0a54d767d7277feeee7a11daa5617cbe36
Nozistance <nozistance@gmail.com>
2024-04-24 01:09:11 +0300

    Remove 'View' from 'Model' in UserData

--- src/main/java/ru/sixbeans/meetingengine/model/UserData.java
+++ src/main/java/ru/sixbeans/meetingengine/model/UserData.java
@@ -2,13 +2,11 @@ package ru.sixbeans.meetingengine.model;
 
 import ru.sixbeans.meetingengine.entity.User;
 
-import java.util.Base64;
-
 public record UserData(
         String email,
         String fullName,
         String userName,
-        String avatar
+        byte[] avatar
 ) {
 
     public static UserData from(User user) {
@@ -16,7 +14,7 @@ public record UserData(
                 user.getEmail(),
                 user.getFullName(),
                 user.getUserName(),
-                "data:image/jpeg;base64," + Base64.getEncoder().encodeToString(user.getAvatar())
+                user.getAvatar()
         );
     }
 }
