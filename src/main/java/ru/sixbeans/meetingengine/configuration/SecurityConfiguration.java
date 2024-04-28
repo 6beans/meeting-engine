34d39f76ee60554fc8fbe3f1daecb0fffdbefbf2
Nozistance <nozistance@gmail.com>
2024-04-28 13:37:23 +0300

    Add alerts to login page

--- src/main/java/ru/sixbeans/meetingengine/configuration/SecurityConfiguration.java
+++ src/main/java/ru/sixbeans/meetingengine/configuration/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package ru.sixbeans.meetingengine.configuration;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,21 +11,23 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import ru.sixbeans.meetingengine.repository.UserRepository;
 
 import static org.springframework.security.web.csrf.CookieCsrfTokenRepository.withHttpOnlyFalse;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
+    private final UserRepository userRepository;
     private final OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(a -> a
-                        .requestMatchers("/", "/", "/login", "/webjars/**").permitAll()
+                        .requestMatchers("/login", "/css/**", "/js/**", "/webjars/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2Login(oauth2Login -> oauth2Login
                         .loginPage("/login")
@@ -32,13 +35,10 @@ public class SecurityConfiguration {
                         .userInfoEndpoint(it -> it.oidcUserService(oidcUserService))
                         .failureHandler(authenticationFailureHandler()))
                 .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/")
                         .invalidateHttpSession(true)
                         .clearAuthentication(true)
                         .deleteCookies("JSESSIONID")
-                        .permitAll()
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")))
+                        .permitAll())
                 .csrf(c -> c.csrfTokenRepository(withHttpOnlyFalse()))
                 .build();
     }
@@ -47,7 +47,8 @@ public class SecurityConfiguration {
     public AuthenticationFailureHandler authenticationFailureHandler() {
         return (request, response, exception) -> {
             request.getSession().setAttribute("flash", "Authentication failed");
-            response.sendRedirect("/login?error=true");
+            log.error(exception.getMessage(), exception);
+            response.sendRedirect("/login?error");
         };
     }
 }
