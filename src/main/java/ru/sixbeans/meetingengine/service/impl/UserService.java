f3d2e586a55a927756de473fba8d62b22bdd9a1b
Nozistance <nozistance@gmail.com>
2024-04-30 17:17:35 +0300

    Add findByToken method to UserService

--- src/main/java/ru/sixbeans/meetingengine/service/impl/UserService.java
+++ src/main/java/ru/sixbeans/meetingengine/service/impl/UserService.java
@@ -1,11 +1,27 @@
 package ru.sixbeans.meetingengine.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.sixbeans.meetingengine.entity.Tag;
 import ru.sixbeans.meetingengine.entity.User;
+import ru.sixbeans.meetingengine.repository.UserRepository;
+
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Optional<User> findByToken(OAuth2AuthenticationToken token) {
+        var provider = token.getAuthorizedClientRegistrationId();
+        var externalId = token.getPrincipal().getName();
+        return userRepository.findByProviderAndExternalId(provider, externalId);
+    }
 
     public void addTag(User user, Tag tag) {
         tag.getUsers().add(user);
