409f3a523ca3c6c1dac6a48c39f5715f31e3081c
Artem Pestrikov <user873475320@gmail.com>
2024-05-02 00:00:21 +0300

    Add FriendData model

---
+++ src/main/java/ru/sixbeans/meetingengine/model/FriendData.java
@@ -0,0 +1,24 @@
+package ru.sixbeans.meetingengine.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FriendData{
+    private Long id;
+    private String email;
+    private String fullName;
+    private String userName;
+    private byte[] avatar;
+    private String profileDescription;
+    private String tgLink;
+    private String vkLink;
+    private Set<TagData> tags;
+}
