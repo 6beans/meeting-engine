bdae329c1a3ce43ce0b65e5f010a9aeeda51edff
ion <ion2005@mail.ru>
2024-05-02 20:24:40 +0300

    Update TagData and UserData

--- src/main/java/ru/sixbeans/meetingengine/model/TagData.java
+++ src/main/java/ru/sixbeans/meetingengine/model/TagData.java
@@ -1,16 +1,21 @@
 package ru.sixbeans.meetingengine.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class TagData {
-    private Long id;
-    private String title;
-    private String category;
+import ru.sixbeans.meetingengine.entity.Tag;
+
+import java.util.stream.Collectors;
+
+public record TagData(
+        Long id,
+        String title,
+        String category
+) {
+    public static TagData from(Tag tag) {
+        return new TagData(
+                tag.getId(),
+                tag.getTitle(),
+                tag.getCategory()
+        );
+    }
+
 }
--- src/main/java/ru/sixbeans/meetingengine/model/UserData.java
+++ src/main/java/ru/sixbeans/meetingengine/model/UserData.java
@@ -4,22 +4,34 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import ru.sixbeans.meetingengine.entity.Event;
+import ru.sixbeans.meetingengine.entity.User;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserData{
-    private Long id;
-    private String email;
-    private String fullName;
-    private String userName;
-    private byte[] avatar;
-    private String profileDescription;
-    private String tgLink;
-    private String vkLink;
-    private Set<TagData> tags;
-    private Set<FriendData> friends;
+public record UserData(
+        Long id,
+        String email,
+        String fullName,
+        String userName,
+        byte[] avatar,
+        String profileDescription,
+        String tgLink,
+        String vkLink,
+        Set<TagData> tags
+){
+    public static UserData from(User user) {
+        return new UserData(
+                user.getId(),
+                user.getEmail(),
+                user.getFullName(),
+                user.getUserName(),
+                user.getAvatar(),
+                user.getProfileDescription(),
+                user.getUserContacts().getTelegramLink(),
+                user.getUserContacts().getVkLink(),
+                user.getTags().stream().map(TagData::from).collect(Collectors.toSet())
+        );
+    }
 }
