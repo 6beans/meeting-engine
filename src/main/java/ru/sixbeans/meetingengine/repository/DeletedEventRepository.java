b124bbb1577b49f3095796424a042e87aff2e0bc
ion <ion2005@mail.ru>
2024-05-05 00:06:34 +0300

    Delete DeletedEvent and DeletedEventRepository

--- src/main/java/ru/sixbeans/meetingengine/repository/DeletedEventRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.sixbeans.meetingengine.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.sixbeans.meetingengine.entity.DeletedEvent;
-
-public interface DeletedEventRepository extends JpaRepository<DeletedEvent, Long> {
-}
