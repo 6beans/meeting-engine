03a7c5685a7f3a48c77cb973957dcea8bcc8c993
ion <ion2005@mail.ru>
2024-05-05 00:13:53 +0300

    Update EventService

--- src/main/java/ru/sixbeans/meetingengine/service/impl/event/EventService.java
+++ src/main/java/ru/sixbeans/meetingengine/service/impl/event/EventService.java
@@ -1,65 +1,86 @@
 package ru.sixbeans.meetingengine.service.impl.event;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.sixbeans.meetingengine.entity.DeletedEvent;
 import ru.sixbeans.meetingengine.entity.Event;
 import ru.sixbeans.meetingengine.entity.User;
+import ru.sixbeans.meetingengine.exception.EventInactiveException;
 import ru.sixbeans.meetingengine.exception.EventNotFoundException;
-import ru.sixbeans.meetingengine.repository.DeletedEventRepository;
 import ru.sixbeans.meetingengine.repository.EventRepository;
 import ru.sixbeans.meetingengine.repository.UserRepository;
 import ru.sixbeans.meetingengine.service.impl.user.UserService;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
 public class EventService {
     private final EventRepository eventRepository;
     private final UserService userService;
-    private final DeletedEventRepository deletedEventRepository;
 
-    public EventService(EventRepository eventRepository, UserRepository userRepository, UserService userService, DeletedEventRepository deletedEventRepository) {
+    public EventService(EventRepository eventRepository, UserRepository userRepository, UserService userService) {
         this.eventRepository = eventRepository;
         this.userService = userService;
-        this.deletedEventRepository = deletedEventRepository;
     }
-
 
     public Event findById(long id) throws EventNotFoundException {
         return eventRepository.findById(id)
                 .orElseThrow(() -> new EventNotFoundException("Event with id = " + id + " not found"));
     }
 
+    public List<Event> findByIsActive(boolean isActive) {
+        return eventRepository.findByIsActive(isActive);
+    }
+
+    public Event findByIdAndIsActive(long id, boolean isActive) throws EventNotFoundException {
+        return eventRepository.findByIdAndIsActive(id, isActive)
+                .orElseThrow(() -> new EventNotFoundException("Event with id = " + id + "and status " + (isActive ? "active" : "inactive") + " not found"));
+    }
+
+
     public List<Event> findAll() {
         return eventRepository.findAll();
     }
 
-    public Set<Event> findByOwnerName(String name) {
-        User owner = userService.findByUsername(name);
-        return owner.getOwned();
+
+    public Set<Event> findByOwnerId(long id) {
+        return userService.findByUserId(id).getOwned();
     }
+
+    public Set<Event> findByOwnerIdAndIsActive(long id, boolean isActive) {
+        return findByOwnerId(id).stream().filter(event -> event.getIsActive() == isActive).collect(Collectors.toSet());
+    }
+
 
     public Set<Event> findByOwnerToken(OAuth2AuthenticationToken token) {
-        User owner = userService.findByToken(token);
-        return owner.getOwned();
+        return userService.findByToken(token).getOwned();
     }
+
+    public Set<Event> findByOwnerTokenAndIsActive(OAuth2AuthenticationToken token, boolean isActive) {
+        return findByOwnerToken(token).stream().filter(event -> event.getIsActive() == isActive).collect(Collectors.toSet());
+    }
+
 
     public Set<Event> findByMemberName(String name) {
-        User owner = userService.findByUsername(name);
-        return owner.getParticipated();
+        return userService.findByUsername(name).getParticipated();
     }
 
+    public Set<Event> findByMemberNameAndIsActive(String name, boolean isActive) {
+        return findByMemberName(name).stream().filter(event -> event.getIsActive() == isActive).collect(Collectors.toSet());
+    }
+
+
     public Set<Event> findByMemberToken(OAuth2AuthenticationToken token) {
-        User owner = userService.findByToken(token);
-        return owner.getParticipated();
+        return userService.findByToken(token).getParticipated();
+    }
+
+    public Set<Event> findByMemberTokenAndIsActive(OAuth2AuthenticationToken token, boolean isActive) {
+        return findByMemberToken(token).stream().filter(event -> event.getIsActive() == isActive).collect(Collectors.toSet());
     }
 
 
@@ -68,6 +89,7 @@ public class EventService {
         User owner = userService.findByToken(token);
         owner.getOwned().add(event);
         event.setCreateDate(LocalDate.now());
+        event.setIsActive(true);
         event.setOwner(owner);
         eventRepository.save(event);
     }
@@ -79,8 +101,8 @@ public class EventService {
         updatedEvent.setMembers(oldEvent.getMembers());
         updatedEvent.setOwner(oldEvent.getOwner());
         updatedEvent.setCreateDate(oldEvent.getCreateDate());
-
         updatedEvent.setId(oldEventId);
+        updatedEvent.setIsActive(true);
 
         eventRepository.save(updatedEvent);
     }
@@ -91,29 +113,30 @@ public class EventService {
     }
 
     @Transactional
-    public void joinToEvent(OAuth2AuthenticationToken token, long eventId) {
+    public void joinToEvent(OAuth2AuthenticationToken token, long eventId) throws EventInactiveException {
         User user = userService.findByToken(token);
         Event event = findById(eventId);
+
+        if (!event.getIsActive()) throw new EventInactiveException("Event inactive");
 
         event.getMembers().add(user);
     }
 
     @Transactional
-    public void leaveFromEvent(OAuth2AuthenticationToken token, long eventId) {
+    public void leaveFromEvent(OAuth2AuthenticationToken token, long eventId) throws EventInactiveException {
         User user = userService.findByToken(token);
         Event event = findById(eventId);
+
+        if (!event.getIsActive()) throw new EventInactiveException("Event inactive");
 
         event.getMembers().remove(user);
     }
 
     @Transactional
-    public void deleteAllByEndDateAfter(LocalDate localDate) {
+    public void changeConditionAllEventsByEndDate(LocalDate localDate) {
         findAll().stream()
                 .filter((event -> event.getEndDate().isBefore(localDate)))
-                .forEach(event -> {
-                    deletedEventRepository.save(new DeletedEvent(event));
-                    delete(event.getId());
-                });
+                .forEach(event -> event.setIsActive(false));
 
     }
 
@@ -121,6 +144,9 @@ public class EventService {
     public void addMember(long eventId, long userId) throws EventNotFoundException, UsernameNotFoundException {
         Event event = findById(eventId);
         User user = userService.findByUserId(userId);
+
+        if (!event.getIsActive()) throw new EventInactiveException("Event inactive");
+
         event.getMembers().add(user);
     }
 
@@ -128,6 +154,8 @@ public class EventService {
     public void deleteMember(long eventId, long userID) throws EventNotFoundException, UsernameNotFoundException {
         Event event = findById(eventId);
         User user = userService.findByUserId(userID);
+
+        if (!event.getIsActive()) throw new EventInactiveException("Event inactive");
 
         event.getMembers().remove(user);
 
