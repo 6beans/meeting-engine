7bd6f9739d50faa13e9083afd6839c2488a405f5
ion <ion2005@mail.ru>
2024-05-05 00:10:46 +0300

    Update EventRepository (add findByIdAndIsActive and findByIsActive)

--- src/main/java/ru/sixbeans/meetingengine/repository/EventRepository.java
+++ src/main/java/ru/sixbeans/meetingengine/repository/EventRepository.java
@@ -3,5 +3,12 @@ package ru.sixbeans.meetingengine.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.sixbeans.meetingengine.entity.Event;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface EventRepository extends JpaRepository<Event, Long> {
+    Optional<Event> findByIdAndIsActive(long id, boolean isActive);
+
+    List<Event> findByIsActive(boolean isActive);
 }
