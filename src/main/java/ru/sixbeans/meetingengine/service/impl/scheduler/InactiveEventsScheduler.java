a02c3de0c8eb761a4ac49a1b0f454695e8561120
ion <ion2005@mail.ru>
2024-05-05 21:55:43 +0300

    Update InactiveEventsScheduler (change time in scheduler and add deleteEventsOlderThanOneMonth)

--- src/main/java/ru/sixbeans/meetingengine/service/impl/scheduler/InactiveEventsScheduler.java
+++ src/main/java/ru/sixbeans/meetingengine/service/impl/scheduler/InactiveEventsScheduler.java
@@ -1,4 +1,5 @@
 package ru.sixbeans.meetingengine.service.impl.scheduler;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ru.sixbeans.meetingengine.service.impl.event.EventService;
@@ -16,8 +17,9 @@ public class InactiveEventsScheduler {
     }
 
 
-    @Scheduled(cron = "0 0 0 * * *")  // Ежедневно в полночь
+    @Scheduled(cron = "0 0 1 * * *")  // Ежедневно в полночь
     public void cleanupEvents() {
         eventService.changeConditionAllEventsByEndDate(LocalDate.now());
+        eventService.deleteEventsOlderThan(LocalDate.now().minusMonths(1));
     }
 }
