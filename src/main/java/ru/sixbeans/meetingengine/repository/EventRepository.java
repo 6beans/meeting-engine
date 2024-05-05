bd36f72986e7af027aeb952ee8183b213d97d0ff
ion <ion2005@mail.ru>
2024-05-05 21:52:16 +0300

    Update EventRepository (add method deleteEventsOlderThan())

--- src/main/java/ru/sixbeans/meetingengine/repository/EventRepository.java
+++ src/main/java/ru/sixbeans/meetingengine/repository/EventRepository.java
@@ -1,8 +1,11 @@
 package ru.sixbeans.meetingengine.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import ru.sixbeans.meetingengine.entity.Event;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +14,8 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     Optional<Event> findByIdAndIsActive(long id, boolean isActive);
 
     List<Event> findByIsActive(boolean isActive);
+
+    @Modifying
+    @Query("DELETE FROM Event e WHERE e.endDate < :thresholdDate")
+    void deleteEventsOlderThan(LocalDate thresholdDate);
 }
