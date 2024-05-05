f249d156cf40001626cfd318642b794ee4e3b940
ion <ion2005@mail.ru>
2024-05-05 21:12:50 +0300

    Add TagRepository

--- src/main/java/ru/sixbeans/meetingengine/repository/TagRepository.java
+++ src/main/java/ru/sixbeans/meetingengine/repository/TagRepository.java
@@ -3,6 +3,9 @@ package ru.sixbeans.meetingengine.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.sixbeans.meetingengine.entity.Tag;
 
+import java.util.Optional;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
+    Optional<Tag> findByTitle(String title);
 }
