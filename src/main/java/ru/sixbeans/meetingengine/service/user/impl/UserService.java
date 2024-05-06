d4cf541def2c7fa0ef2881bdf5f9158427ae4680
Nozistance <nozistance@gmail.com>
2024-05-06 17:54:29 +0300

    Refactor UserService

--- src/main/java/ru/sixbeans/meetingengine/service/user/impl/UserService.java
+++ src/main/java/ru/sixbeans/meetingengine/service/user/impl/UserService.java
@@ -1,116 +1,93 @@
 package ru.sixbeans.meetingengine.service.user.impl;
 
-import org.hibernate.Hibernate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.sixbeans.meetingengine.entity.Tag;
 import ru.sixbeans.meetingengine.entity.User;
-import ru.sixbeans.meetingengine.exception.TagNotFoundException;
-import ru.sixbeans.meetingengine.repository.TagRepository;
+import ru.sixbeans.meetingengine.exception.UserNotFoundException;
+import ru.sixbeans.meetingengine.mapper.UserMapper;
+import ru.sixbeans.meetingengine.model.PersonalInfoData;
+import ru.sixbeans.meetingengine.model.UserData;
 import ru.sixbeans.meetingengine.repository.UserRepository;
 
-import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
-@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserMapper mapper;
     private final UserRepository userRepository;
-    private final TagRepository tagRepository;
 
-    @Autowired
-    public UserService(UserRepository userRepository, TagRepository tagRepository) {
-        this.userRepository = userRepository;
-        this.tagRepository = tagRepository;
+    @Transactional(readOnly = true)
+    public UserData getUserByUsername(String userName) {
+        var user = userRepository.findByUserName(userName);
+        if (user.isEmpty() || !userName.startsWith("@"))
+            throw new UserNotFoundException(userName);
+        else return mapper.map(user.get());
     }
 
-    public User findByToken(OAuth2AuthenticationToken token) {
-        var provider = token.getAuthorizedClientRegistrationId();
-        var externalId = token.getPrincipal().getName();
-
-        return userRepository.findByProviderAndExternalId(provider, externalId)
-                .orElseThrow(() -> new UsernameNotFoundException("User with the token was not found"));
+    @Transactional(readOnly = true)
+    public UserData getUserById(long userId) {
+        return mapper.map(userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId)));
     }
 
-    public User findByUsername(String name) {
-        return userRepository.findByUserName(name)
-                .orElseThrow(() -> new UsernameNotFoundException("User with username = " + name + " was not found"));
+    @Transactional(readOnly = true)
+    public PersonalInfoData getUserPersonalInfoById(long userId) {
+        return mapper.map(userRepository.findById(userId).map(User::getPersonalInfo)
+                .orElseThrow(() -> new UserNotFoundException(userId)));
     }
 
-    public User findByUserId(long id) {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new UsernameNotFoundException("User with username = " + id + " was not found"));
+    @Transactional(readOnly = true)
+    public List<UserData> findAllUserSubscriptions(long userId) {
+        return mapper.map(userRepository.getReferenceById(userId).getSubscriptions());
     }
 
-    public Set<User> findAll() {
-        return new HashSet<>(userRepository.findAll());
+    @Transactional(readOnly = true)
+    public List<UserData> findAllUserSubscribers(long userId) {
+        return mapper.map(userRepository.getReferenceById(userId).getSubscribers());
     }
 
-    public Set<User> findUserFriends(String name) {
-        return findByUsername(name).getFriends();
+    @Transactional(readOnly = true)
+    public boolean isSubscriber(long userId, long subscriberId) {
+        var subscribers = userRepository.getReferenceById(userId).getSubscribers();
+        var subscriber = userRepository.getReferenceById(subscriberId);
+        return subscribers.contains(subscriber);
     }
 
-    public boolean isFriend(long userId, long friendId) {
-        User user = findByUserId(userId);
-        User friend = findByUserId(friendId);
-
-        Hibernate.initialize(user.getFriends());
-
-        return user.getFriends().contains(friend);
-    }
-
-
-    public Set<User> getFullFriendsByUserName(String name) {
-        User user = findByUsername(name);
-
-        return findByUsername(name).getFriends()
-                .stream()
-                .peek(friend -> Hibernate.initialize(friend.getFriends()))
-                .filter((friend) -> friend.getFriends().contains(user))
-                .collect(Collectors.toSet());
-
+    @Transactional(readOnly = true)
+    public boolean isSubscription(long userId, long subscriptionId) {
+        var subscriptions = userRepository.getReferenceById(userId).getSubscriptions();
+        var subscription = userRepository.getReferenceById(subscriptionId);
+        return subscriptions.contains(subscription);
     }
 
     @Transactional
-    public void deleteUser(long userId) {
-        userRepository.deleteById(userId);
+    public void subscribe(long userId, long subscriptionId) {
+        User user = userRepository.getReferenceById(userId);
+        User subscription = userRepository.getReferenceById(subscriptionId);
+        user.getSubscriptions().add(subscription);
+        subscription.getSubscribers().add(user);
     }
 
     @Transactional
-    public void addFriend(long userId, long friendId) {
-        User user = findByUserId(userId);
-        User friend = findByUserId(friendId);
-        user.getFriends().add(friend);
+    public void unsubscribe(long userId, long subscriptionId) {
+        User user = userRepository.getReferenceById(userId);
+        User subscription = userRepository.getReferenceById(subscriptionId);
+        user.getSubscriptions().remove(subscription);
+        subscription.getSubscribers().remove(user);
     }
 
-    @Transactional
-    public void removeFriend(long userId, long friendId) {
-        User user = findByUserId(userId);
-        User friend = findByUserId(friendId);
-        user.getFriends().remove(friend);
-    }
-
-    @Transactional
-    public void addTag(long userId, long tagId) {
-        User user = findByUserId(userId);
-        Tag tag = tagRepository.findById(tagId)
-                .orElseThrow(() -> new TagNotFoundException("Tag with id = " + tagId + " not found"));
-
-        user.getTags().add(tag);
-        tag.getUsers().add(user);
-    }
-
-    @Transactional
-    public void removeTag(long userId, long tagId) {
-        User user = findByUserId(userId);
-        Tag tag = tagRepository.findById(tagId)
-                .orElseThrow(() -> new TagNotFoundException("Tag with id = " + tagId + " not found"));
-
-        user.getTags().remove(tag);
-        tag.getUsers().remove(user);
+    @Transactional(readOnly = true)
+    public List<UserData> findMutualSubscriptions(long userId) {
+        User user = userRepository.getReferenceById(userId);
+        Set<User> subscriptions = user.getSubscriptions();
+        Set<User> subscribers = user.getSubscribers();
+        return subscriptions.stream()
+                .filter(subscribers::contains)
+                .map(mapper::map)
+                .toList();
     }
 }
