2228529e501a7d37a8b0e28a3065652f76a02972
Nozistance <nozistance@gmail.com>
2024-05-06 17:53:30 +0300

    Refactor EventData (DTO)

--- src/main/java/ru/sixbeans/meetingengine/model/EventData.java
+++ src/main/java/ru/sixbeans/meetingengine/model/EventData.java
@@ -1,35 +1,26 @@
 package ru.sixbeans.meetingengine.model;
 
-import lombok.Data;
-import org.modelmapper.ModelMapper;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotBlank;
 import ru.sixbeans.meetingengine.entity.Event;
-import ru.sixbeans.meetingengine.entity.Tag;
-import ru.sixbeans.meetingengine.entity.User;
 
+import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Collection;
 
-public record EventData(
-        String name,
-        String description,
-        Long id,
-        Set<Tag> tags,
-        UserData owner,
-        Set<UserData> members,
-        LocalDate createDate,
-        LocalDate endDate
-) {
-    public static EventData from(Event event) {
-        return new EventData(
-                event.getName(),
-                event.getDescription(),
-                event.getId(),
-                event.getTags(),
-                UserData.from(event.getOwner()),
-                event.getMembers().stream().map(UserData::from).collect(Collectors.toSet()),
-                event.getCreateDate(),
-                event.getEndDate()
-        );
-    }
+/**
+ * DTO for {@link Event}
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record EventData(Long id,
+                        Long ownerId,
+                        @NotBlank String title,
+                        String description,
+                        LocalDate creationDate,
+                        LocalDate endDate,
+                        Boolean isActive,
+                        Collection<Long> tagIds,
+                        Collection<Long> memberIds)
+        implements Serializable {
+
 }
