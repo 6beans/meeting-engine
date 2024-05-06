2f63ed3b3827c602a54b87c9fcd559ea82f3b3d4
Nozistance <nozistance@gmail.com>
2024-05-06 17:47:44 +0300

    Remove unused methods from EventRepository

--- src/main/java/ru/sixbeans/meetingengine/repository/EventRepository.java
+++ src/main/java/ru/sixbeans/meetingengine/repository/EventRepository.java
@@ -1,21 +1,13 @@
 package ru.sixbeans.meetingengine.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import ru.sixbeans.meetingengine.entity.Event;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
-    Optional<Event> findByIdAndIsActive(long id, boolean isActive);
 
-    List<Event> findByIsActive(boolean isActive);
+    List<Event> findAllByIsActive(boolean isActive);
 
-    @Modifying
-    @Query("DELETE FROM Event e WHERE e.endDate < :thresholdDate")
-    void deleteEventsOlderThan(LocalDate thresholdDate);
+    List<Event> findAllByOwnerId(long id);
 }
