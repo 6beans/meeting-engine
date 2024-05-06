7d6d66e2f1a60e95f78afafdaf01887c29838746
Nozistance <nozistance@gmail.com>
2024-05-06 17:54:05 +0300

    Add TagMapper

--- src/main/java/ru/sixbeans/meetingengine/repository/TagRepository.java
+++ src/main/java/ru/sixbeans/meetingengine/repository/TagRepository.java
@@ -3,9 +3,6 @@ package ru.sixbeans.meetingengine.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.sixbeans.meetingengine.entity.Tag;
 
-import java.util.Optional;
-
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
-    Optional<Tag> findByTitle(String title);
 }
