6bbdd6de3bfb1e332055c7917b648e15e4312555
Nozistance <nozistance@gmail.com>
2024-05-06 17:51:38 +0300

    Remove unused findByEmail method from UserRepository

--- src/main/java/ru/sixbeans/meetingengine/repository/UserRepository.java
+++ src/main/java/ru/sixbeans/meetingengine/repository/UserRepository.java
@@ -7,8 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
-
     Optional<User> findByUserName(String username);
 
     Optional<User> findByProviderAndExternalId(String provider, String externalId);
