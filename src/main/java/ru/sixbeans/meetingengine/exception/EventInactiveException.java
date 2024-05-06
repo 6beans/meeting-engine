35a22867277622075507ce599cf364a2df686442
Nozistance <nozistance@gmail.com>
2024-05-06 17:48:15 +0300

    Remove unused EventInactiveException

--- src/main/java/ru/sixbeans/meetingengine/exception/EventInactiveException.java
+++
@@ -1,7 +0,0 @@
-package ru.sixbeans.meetingengine.exception;
-
-public class EventInactiveException extends RuntimeException {
-    public EventInactiveException(String message) {
-        super(message);
-    }
-}
