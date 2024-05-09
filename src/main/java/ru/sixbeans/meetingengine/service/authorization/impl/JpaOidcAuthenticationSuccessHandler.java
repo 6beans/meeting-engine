b32542ace5fb042a8d88980154fe4b6212f37a80
Nozistance <nozistance@gmail.com>
2024-05-09 11:23:13 +0300

    Add OidcUserService to work with User creation on first login

--- src/main/java/ru/sixbeans/meetingengine/service/authorization/impl/JpaOidcAuthenticationSuccessHandler.java
+++ src/main/java/ru/sixbeans/meetingengine/service/authorization/impl/JpaOidcAuthenticationSuccessHandler.java
@@ -7,50 +7,25 @@ import lombok.SneakyThrows;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import ru.sixbeans.meetingengine.entity.PersonalInfo;
-import ru.sixbeans.meetingengine.entity.User;
-import ru.sixbeans.meetingengine.repository.UserRepository;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class JpaOidcAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
-    private final OidcProfilePictureService profilePictureService;
-    private final UserRepository userRepository;
+    private final OidcUserService userService;
 
     @Override
     @SneakyThrows
     @Transactional
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        if (authentication.getPrincipal() instanceof OidcUser user) createUserIfNotExist(user);
-        else throw new IllegalStateException("Unsupported authentication type");
+        if (authentication.getPrincipal() instanceof OidcUser user) {
+            Long userId = userService.createUserIfNotExist(user);
+            request.getSession().setAttribute("userId", userId);
+        } else {
+            throw new IllegalStateException("Unsupported authentication type");
+        }
         response.sendRedirect("/");
-    }
-
-    private void createUserIfNotExist(OidcUser principal) {
-        String provider = principal.getIssuer().getHost();
-        String subject = principal.getSubject();
-        if (userRepository.existsByProviderAndSubject(provider, subject)) return;
-
-        var profilePicture = profilePictureService
-                .getProfilePicture(principal);
-
-        User user = new User();
-
-        user.setProvider(provider);
-        user.setUserName(subject);
-        user.setSubject(subject);
-        user.setEmail(principal.getEmail());
-        profilePicture.ifPresent(user::setAvatar);
-
-        PersonalInfo personalInfo = new PersonalInfo();
-        personalInfo.setFullName(principal.getFullName());
-        personalInfo.setUser(user);
-
-        user.setPersonalInfo(personalInfo);
-
-        userRepository.save(user);
     }
 }
