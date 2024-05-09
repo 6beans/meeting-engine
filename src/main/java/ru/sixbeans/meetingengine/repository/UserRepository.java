8e42c43b2c3c62735da4df482551aadba20a7310
Shcherbanev Konstantin <shcherbanev1@yandex.ru>
2024-05-09 22:13:56 +0300

    add recommendation and filter query for users

--- src/main/java/ru/sixbeans/meetingengine/repository/UserRepository.java
+++ src/main/java/ru/sixbeans/meetingengine/repository/UserRepository.java
@@ -1,26 +1,41 @@
 package ru.sixbeans.meetingengine.repository;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.sixbeans.meetingengine.entity.User;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    boolean existsBySignedSub(String signedSub);
+    Optional<User> findByUserName(String username);
 
-    Optional<User> findByEmail(String email);
+    boolean existsByProviderAndSubject(String provider, String subject);
 
-    @Query(value = "SELECT u.*, COUNT(ut.tags_id) AS common_tags " +
-            "FROM users_tags ut " +
-            "JOIN users u ON ut.users_id = u.id " +
-            "WHERE ut.tags_id IN (SELECT tags_id FROM users_tags WHERE users_id = :userId) " +
-            "AND ut.users_id != :userId " +
-            "GROUP BY u.id " +
-            "ORDER BY common_tags DESC;", nativeQuery = true)
+    Optional<User> findByProviderAndSubject(String provider, String subject);
+
+    boolean existsByIdAndSubscribers_Id(Long userId, Long subscriberId);
+
+    boolean existsByIdAndSubscriptions_Id(Long userId, Long subscriberId);
+
+    @Query(value = "SELECT users.*" +
+            "FROM tag_users" +
+            "JOIN users ON tag_users.users_id = users.id" +
+            "WHERE tag_users.tags_id IN (SELECT tags_id FROM tag_users WHERE users_id = :userId)" +
+            "AND tag_users.users_id != :userId" +
+            "GROUP BY users.id" +
+            "ORDER BY  COUNT(tag_users.tags_id) DESC", nativeQuery = true)
     List<User> getRecommendedUsers(@Param("userId") Long userId);
+
+    @Query(value = "SELECT users.* " +
+            "FROM users " +
+            "JOIN tag_users ON users.id = tag_users.users_id " +
+            "WHERE tag_users.tags_id IN :tags " +
+            "GROUP BY users.id " +
+            "ORDER BY COUNT(tag_users.tags_id) DESC", nativeQuery = true)
+    List<User> filterUsersByTags(@Param("tags") Set<Integer> tags);
 
 }
