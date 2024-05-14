2c30136a777754e5c74e01808f7ebe06ab36f1e1
Nozistance <nozistance@gmail.com>
2024-05-14 16:19:51 +0300

    Add H2 Database for tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
+    runtimeOnly("com.h2database:h2")
     testRuntimeOnly("com.h2database:h2")
     implementation("org.mapstruct:mapstruct:1.4.2.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.4.2.Final")
