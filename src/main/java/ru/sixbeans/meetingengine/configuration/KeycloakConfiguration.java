8bd1cba8e8574be6dd186d18c7d9f6c47dd7888e
Nozistance <nozistance@gmail.com>
2024-06-04 20:05:19 +0300

    Add Keycloak logout page

--- src/main/java/ru/sixbeans/meetingengine/configuration/KeycloakConfiguration.java
+++ src/main/java/ru/sixbeans/meetingengine/configuration/KeycloakConfiguration.java
@@ -1,22 +1,34 @@
 package ru.sixbeans.meetingengine.configuration;
 
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RequiredArgsConstructor
+@Getter
+@SuppressWarnings("unused")
 @ConfigurationProperties("keycloak")
 public class KeycloakConfiguration {
 
     private final String password;
     private final String username;
     private final String serverUrl;
+    private final String logoutUrl;
+
+    public KeycloakConfiguration(String realm, String password, String username, String serverUrl) {
+        this.password = password;
+        this.username = username;
+        this.serverUrl = serverUrl;
+        logoutUrl = "%s/realms/%s/protocol/openid-connect/logout"
+                .formatted(serverUrl, realm);
+    }
 
     @Bean
     public Keycloak keycloak() {
+        System.out.println(logoutUrl);
         return KeycloakBuilder.builder().serverUrl(serverUrl)
                 .realm("master").clientId("admin-cli")
                 .username(username).password(password)
--- src/main/java/ru/sixbeans/meetingengine/configuration/SecurityConfiguration.java
+++ src/main/java/ru/sixbeans/meetingengine/configuration/SecurityConfiguration.java
@@ -6,14 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 import static org.springframework.security.web.csrf.CookieCsrfTokenRepository.withHttpOnlyFalse;
 
@@ -21,35 +16,31 @@ import static org.springframework.security.web.csrf.CookieCsrfTokenRepository.wi
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 259200) // 3 days
 public class SecurityConfiguration {
 
+    private final KeycloakConfiguration keycloakConfiguration;
     private final AuthenticationSuccessHandler successHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login", "/css/**", "/js/**", "/webjars/**")
-                        .permitAll().anyRequest().authenticated())
-                .sessionManagement(s -> s.maximumSessions(1))
-                .oauth2Login(oauth2 -> oauth2.loginPage("/login")
-                        .userInfoEndpoint(u -> u.oidcUserService(oidcUserService()))
-                        .successHandler(successHandler)
-                        .failureHandler(failureHandler()))
-                .logout(logout -> logout
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
-                        .deleteCookies("JSESSIONID")
-                        .permitAll())
-                .csrf(c -> c.csrfTokenRepository(withHttpOnlyFalse()));
-
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/login", "/logout", "/css/**", "/js/**", "/webjars/**")
+                .permitAll().anyRequest().authenticated());
+        http.oauth2Login(oauth2 -> oauth2.loginPage("/login")
+                .successHandler(successHandler)
+                .failureHandler(failureHandler()));
+        http.logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessHandler((request, response, authentication) -> {
+                    response.sendRedirect(keycloakConfiguration.getLogoutUrl());
+                })
+        );
+        http.csrf(c -> c.csrfTokenRepository(withHttpOnlyFalse()));
         return http.build();
-    }
-
-    @Bean
-    public OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
-        return new OidcUserService();
     }
 
     @Bean
