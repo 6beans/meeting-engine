8397e027130a0e6dc55e6df4392b1f8cb91c4a4d
Nozistance <nozistance@gmail.com>
2024-06-06 11:48:35 +0300

    Change user's ID type in UserService

--- src/main/java/ru/sixbeans/meetingengine/service/user/impl/UserService.java
+++ src/main/java/ru/sixbeans/meetingengine/service/user/impl/UserService.java
@@ -5,71 +5,55 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.sixbeans.meetingengine.entity.User;
 import ru.sixbeans.meetingengine.exception.UserNotFoundException;
-import ru.sixbeans.meetingengine.mapper.UserMapper;
-import ru.sixbeans.meetingengine.model.UserData;
 import ru.sixbeans.meetingengine.repository.UserRepository;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserMapper mapper;
     private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
-    public UserData getUser(long userId) {
-        return userRepository.findById(userId).map(mapper::map)
-                .orElseThrow(() -> new UserNotFoundException(userId));
-    }
-
-    @Transactional(readOnly = true)
-    public Collection<UserData> getUsers(Collection<Long> userIds) {
-        return userIds.stream().map(userRepository::findById)
-                .flatMap(Optional::stream).map(mapper::map)
-                .toList();
-    }
-
-    @Transactional(readOnly = true)
-    public byte[] getUserAvatar(long userId) {
-        return userRepository.getReferenceById(userId)
+    public byte[] getUserAvatar(String subject) {
+        return userRepository.getReferenceById(subject)
                 .getAvatar();
     }
 
     @Transactional
-    public void updateUserAvatar(long userId, byte[] avatar) {
-        userRepository.getReferenceById(userId)
+    public void updateUserAvatar(String subject, byte[] avatar) {
+        userRepository.getReferenceById(subject)
                 .setAvatar(avatar);
     }
 
     @Transactional(readOnly = true)
-    public List<UserData> findAllUserSubscriptions(long userId) {
+    public List<String> findAllUserSubscriptions(String userId) {
         return userRepository.findById(userId).map(User::getSubscriptions)
-                .map(mapper::map).orElseThrow(() -> new UserNotFoundException(userId));
+                .orElseThrow(() -> new UserNotFoundException(userId))
+                .stream().map(User::getId).toList();
     }
 
     @Transactional(readOnly = true)
-    public List<UserData> findAllUserSubscribers(long userId) {
-        return userRepository.findById(userId).map(User::getSubscribers)
-                .map(mapper::map).orElseThrow(() -> new UserNotFoundException(userId));
+    public List<String> findAllUserSubscribers(String userId) {
+        return userRepository.getReferenceById(userId)
+                .getSubscribers().stream()
+                .map(User::getId).toList();
     }
 
     @Transactional(readOnly = true)
-    public boolean isSubscriber(long userId, long subscriberId) {
+    public boolean isSubscriber(String userId, String subscriberId) {
         return userRepository.existsByIdAndSubscribers_Id(userId, subscriberId);
     }
 
     @Transactional(readOnly = true)
-    public boolean isSubscription(long userId, long subscriptionId) {
+    public boolean isSubscription(String userId, String subscriptionId) {
         return userRepository.existsByIdAndSubscriptions_Id(userId, subscriptionId);
     }
 
     @Transactional
-    public void subscribe(long userId, long subscriptionId) {
+    public void subscribe(String userId, String subscriptionId) {
         User user = userRepository.getReferenceById(userId);
         User subscription = userRepository.getReferenceById(subscriptionId);
         user.getSubscriptions().add(subscription);
@@ -77,7 +61,7 @@ public class UserService {
     }
 
     @Transactional
-    public void unsubscribe(long userId, long subscriptionId) {
+    public void unsubscribe(String userId, String subscriptionId) {
         User user = userRepository.getReferenceById(userId);
         User subscription = userRepository.getReferenceById(subscriptionId);
         user.getSubscriptions().remove(subscription);
@@ -85,13 +69,13 @@ public class UserService {
     }
 
     @Transactional(readOnly = true)
-    public List<UserData> findMutualSubscriptions(long userId) {
+    public List<String> findMutualSubscriptions(String userId) {
         User user = userRepository.getReferenceById(userId);
         Set<User> subscriptions = user.getSubscriptions();
         Set<User> subscribers = user.getSubscribers();
         return subscriptions.stream()
                 .filter(subscribers::contains)
-                .map(mapper::map)
+                .map(User::getId)
                 .toList();
     }
 }
