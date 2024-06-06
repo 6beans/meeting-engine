2575cfda0cb171c665a08f4d50537c4d6b55657d
Nozistance <nozistance@gmail.com>
2024-06-06 11:29:59 +0300

    Remove UserProfileData, now use UserData

--- src/main/java/ru/sixbeans/meetingengine/model/UserData.java
+++ src/main/java/ru/sixbeans/meetingengine/model/UserData.java
@@ -1,7 +1,6 @@
 package ru.sixbeans.meetingengine.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.validation.constraints.NotBlank;
 import ru.sixbeans.meetingengine.entity.User;
 
 import java.io.Serializable;
@@ -10,11 +9,9 @@ import java.io.Serializable;
  * DTO for {@link User}
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record UserData(Long id,
-                       @NotBlank String email,
-                       @NotBlank String userName,
-                       @NotBlank String fullName,
-                       String profileDescription)
+public record UserData(String subject, String userName,
+                       String firstName, String secondName,
+                       String email, String about)
         implements Serializable {
 
 }
