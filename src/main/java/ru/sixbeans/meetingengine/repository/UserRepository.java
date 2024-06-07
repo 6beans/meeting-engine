9a1f083323b2d28b327988b2c4d60c7dcc55daca
Nozistance <nozistance@gmail.com>
2024-06-07 11:56:15 +0300

    Add findMostSuitableUsersByTags method to UserRepository

--- src/main/java/ru/sixbeans/meetingengine/repository/UserRepository.java
+++ src/main/java/ru/sixbeans/meetingengine/repository/UserRepository.java
@@ -1,11 +1,18 @@
 package ru.sixbeans.meetingengine.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.sixbeans.meetingengine.entity.User;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, String> {
 
     boolean existsByIdAndSubscribers_Id(String userId, String subscriberId);
 
     boolean existsByIdAndSubscriptions_Id(String userId, String subscriberId);
+
+    @Query("SELECT u FROM User u JOIN u.tags t WHERE t.id IN :tags GROUP BY u ORDER BY COUNT(t) DESC")
+    List<User> findMostSuitableUsersByTags(@Param("tags") List<String> tags);
 }
