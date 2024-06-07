3d20fa5d95de3310637429bd899ea324fbe210ab
Nozistance <nozistance@gmail.com>
2024-06-07 13:18:16 +0300

    Create IndexController

--- src/main/java/ru/sixbeans/meetingengine/controller/IndexController.java
+++ src/main/java/ru/sixbeans/meetingengine/controller/IndexController.java
@@ -2,14 +2,12 @@ package ru.sixbeans.meetingengine.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
-public class LoginController {
+public class IndexController {
 
     @GetMapping
-    public String login() {
-        return "login";
+    public String index() {
+        return "index";
     }
 }
