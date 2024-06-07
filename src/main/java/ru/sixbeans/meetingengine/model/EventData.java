a9494343270b8c248c41c82a65c89ff2f3fcfc8a
Nozistance <nozistance@gmail.com>
2024-06-07 11:57:38 +0300

    Change types of properties

--- src/main/java/ru/sixbeans/meetingengine/model/EventData.java
+++ src/main/java/ru/sixbeans/meetingengine/model/EventData.java
@@ -1,22 +1,24 @@
 package ru.sixbeans.meetingengine.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.validation.constraints.NotBlank;
 import ru.sixbeans.meetingengine.entity.Event;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * DTO for {@link Event}
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record EventData(Long id, String ownerId,
-                        @NotBlank String title,
-                        String description,
-                        LocalDate creationDate,
-                        LocalDate endDate,
+                        String title, String about,
+                        LocalDateTime startDate,
+                        LocalDateTime endDate,
                         Boolean isActive)
         implements Serializable {
 
+    public static EventData of(String ownerId) {
+        return new EventData(null, ownerId, "", "", null, null, true);
+    }
 }
